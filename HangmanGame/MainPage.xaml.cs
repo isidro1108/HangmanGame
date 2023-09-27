3305875ba72c316367fbf19a291536f214213942
ImDev11 <euris110811@gmail.com>
2023-09-27 10:30:08 -0400

    feat: bindable property for the word

--- HangmanGame/MainPage.xaml.cs
+++ HangmanGame/MainPage.xaml.cs
@@ -4,6 +4,18 @@ namespace HangmanGame
 {
     public partial class MainPage : ContentPage, INotifyPropertyChanged
     {
+        #region UI Properties
+        public string Spotlight
+        {
+            get => spotlight;
+            set
+            {
+                spotlight = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
         #region Fields
         List<string> words = new List<string>()
         {
@@ -14,17 +26,29 @@ namespace HangmanGame
             "mongodb"
         };
         string answer = string.Empty;
+        private string spotlight;
+        List<char> guessed = new List<char>();
         #endregion
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
             PickWord();
+            CalculateWord(answer, guessed);
         }
 
         #region Game Engine
         private void PickWord()
         {
             answer = words[new Random().Next(0, words.Count)];
+        }
+
+        private void CalculateWord(string answer, List<char> guessed)
+        {
+            var temp = answer
+                        .Select(x => guessed.IndexOf(x) >= 0 ? x : '_')
+                        .ToArray();
+            Spotlight = string.Join(' ', temp);
         }
         #endregion
     }
