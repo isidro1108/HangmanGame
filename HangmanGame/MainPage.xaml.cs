4ed800052602d822af14890534e29474536de956
ImDev11 <euris110811@gmail.com>
2023-09-27 10:09:40 -0400

    feat: PickWord method has been created

--- HangmanGame/MainPage.xaml.cs
+++ HangmanGame/MainPage.xaml.cs
@@ -1,10 +1,31 @@
-﻿namespace HangmanGame
+﻿using System.ComponentModel;
+
+namespace HangmanGame
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
     {
+        #region Fields
+        List<string> words = new List<string>()
+        {
+            "python",
+            "javascript",
+            "maui",
+            "csharp",
+            "mongodb"
+        };
+        string answer = string.Empty;
+        #endregion
         public MainPage()
         {
             InitializeComponent();
+            PickWord();
         }
+
+        #region Game Engine
+        private void PickWord()
+        {
+            answer = words[new Random().Next(0, words.Count)];
+        }
+        #endregion
     }
 }
